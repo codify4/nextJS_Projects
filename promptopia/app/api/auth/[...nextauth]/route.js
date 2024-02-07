fe7994ef81ad6038b8f334a3a4ef8f95041d89f5
IjonK007 <kushta.joni@gmail.com>
2024-02-07 20:47:27 +0100

    auth config- some error happening

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 console.log({
     clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -14,13 +17,36 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({ profile }) {
         try{
+            await connectToDB();
 
+            //check if user already exists
+            const userExist = await User.findOne({
+                email: profile.email
+            });
+
+            //if not, create new user
+            if(!userExist){
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+
+            return true;
         } catch (error) {
-            
+            console.log(error);
+            return false;
         }
     }
 })
--- promptopia/components/Nav.jsx
+++ promptopia/components/Nav.jsx
@@ -7,18 +7,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -34,9 +34,11 @@ const Nav = () => {
         <p className='logo_text'> Promptopia</p>
       </Link>
 
+      
+
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>
               Post
@@ -74,7 +76,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
               src="/assets/images/logo.svg"
