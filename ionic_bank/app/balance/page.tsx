85cb1271f2615baaaa38d7a6933a3baa2151c91d
IjonK007 <kushta.joni@gmail.com>
2024-02-11 20:40:52 +0100

    Deposit Card Component design

--- ionic_bank/app/balance/page.tsx
+++ ionic_bank/app/balance/page.tsx
@@ -1,13 +1,47 @@
 "use client"
 import React, { useState } from 'react';
 
+import Button from '../../app/components/Button';
+import Deposit from '../components/Deposit';
+import Withdraw from '../components/Withdraw';
+
 const page = () => {
 
     const [balance, setBalance] = useState(0);
+    const [isDepositVisible, setIsDepositVisible] = useState(false);
+    const [isWithdrawVisible, setIsWithdrawVisible] = useState(false);
+
+    const handleDepositClick = () => {
+        setIsDepositVisible(true);
+    }
+    const handleWithdrawClick = () => {
+        setIsWithdrawVisible(true);
+    }
+    const handleDepositClose = () => {
+        setIsDepositVisible(false);
+    }
+    const handleWithdrawClose = () => {
+        setIsWithdrawVisible(false);
+    }
+    
 
     return (
-        <main className='flex items-start justify-center my-14 mx-14 w-9/12 h-[800px] bg-[#ffffff] rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
-            <h1 className='text-6xl' onChange={() => {setBalance}}>BALANCE: {balance}</h1>
+        <main className='flex flex-col items-center justify-center my-14 mx-14 w-9/12 h-[800px] bg-[#ffffff] rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
+            <h1 className='flex flex-col text-center mt-[-100px] mb-[30px] text-6xl font-bold bg-gradient-to-r from-[#4D4DFF] vie-[#0000CD] to-[#00006B] text-transparent bg-clip-text' onChange={() => {setBalance}}>
+                <span>BALANCE</span>
+                ${balance}
+            </h1>
+
+            <div className='flex'>
+                
+                <Button text='Deposit' onClick={handleDepositClick}/>
+
+                <Button text='Withdraw' onClick={handleWithdrawClick}/>
+                
+            </div>
+
+            {isDepositVisible && <Deposit deposit={handleDepositClose}/>}
+            {isWithdrawVisible && <Withdraw />}
         </main>
     )
 }
--- ionic_bank/app/components/Deposit.tsx
+++ ionic_bank/app/components/Deposit.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const Deposit = () => {
+type DepositProps = {
+    depositedMoney: number;
+    deposit?: () => void;
+}
+
+const Deposit = ({ deposit, depositedMoney }: DepositProps) => {
   return (
-    <div>s</div>
+    <div className='flex flex-col items-center justify-center fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] h-[300px] bg-white p-8 rounded-md shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
+
+        <input 
+            type="number" 
+            placeholder='Deposit money...' 
+            value={depositedMoney}
+            className='m-2 p-2 rounded-md appearance-none border-2 border-[#A9A9A9] text-3xl'
+            />
+
+        <button className='bg-none border-2 border-black text-3xl text-black py-1 px-10 rounded-xl m-5 transition-transform transform  hover:-translate-y-2 hover:bg-gradient-to-r from-[#000080] to-[#0000CD] hover:border-none hover:text-white'
+        onClick={deposit}
+        >
+            Deposit
+        </button>
+    </div>
   )
 }
 
