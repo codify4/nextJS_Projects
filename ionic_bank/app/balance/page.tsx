b2ef311ce2e8bc9102ec112c71c1499d1c63e9af
IjonK007 <kushta.joni@gmail.com>
2024-02-14 18:51:56 +0100

    Currency Component Complete

--- ionic_bank/app/balance/page.tsx
+++ ionic_bank/app/balance/page.tsx
@@ -11,9 +11,10 @@ const page = () => {
 
     const [isDepositVisible, setIsDepositVisible] = useState(false);
     const [isWithdrawVisible, setIsWithdrawVisible] = useState(false);
-    
+
     const [balance, setBalance] = useState(0);
-    
+    const [currency, setCurrency] = useState('');
+
     // Click state for deposit/ withdraw card components
     const handleDepositClick = () => {
         setIsDepositVisible(true);
@@ -27,31 +28,36 @@ const page = () => {
     const handleWithdrawClose = () => {
         setIsWithdrawVisible(false);
     }
-
+    // Update the balance by adding the deposited money
     const handleDepositedMoneyChange = (value?: number) => {
-        // Update the balance by adding the deposited money
         setBalance((b) => b + (value ?? 0));
         handleDepositClose();
     }
+     // Update the balance by subtracting the withdrawn money
     const handleWithdrawnMoneyChange = (value?: number) => {
-        // Update the balance by subtracting the withdrawn money
         setBalance((b) => b - (value ?? 0));
         handleWithdrawClose();
+    }
+
+    const handleCurrencyChange = (currency: string) => {
+        setCurrency(currency);
     }
 
     return (
         <main className='flex flex-col items-center justify-center my-14 mx-14 w-9/12 h-[800px] bg-[#ffffff] rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.12)]'>
             
-            <Currency />
+            <Currency
+                onCurrencyChange={handleCurrencyChange}
+            />
 
             {/*Transparent BG */}
             {(isDepositVisible || isWithdrawVisible) && (
                 <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-60 z-"></div>
             )}
 
-            <h1 className='flex flex-col text-center mt-[-100px] mb-[30px] text-8xl font-bold bg-gradient-to-r from-[#4D4DFF] vie-[#0000CD] to-[#00006B] text-transparent bg-clip-text' onChange={() => {setBalance}}>
-                <span>BALANCE</span>
-                ${balance}
+            <h1 className='flex flex-col text-center mt-[-100px] mb-[30px] text-7xl font-bold bg-gradient-to-r from-[#4D4DFF] vie-[#0000CD] to-[#00006B] text-transparent bg-clip-text' onChange={() => {setBalance}}>
+                <span className='text-[80px]'>BALANCE</span>
+                {`${currency} ${balance}`}
             </h1>
 
             <div className='flex'>
--- ionic_bank/components/Currency.tsx
+++ ionic_bank/components/Currency.tsx
@@ -19,18 +19,15 @@ import {
 } from "@/components/ui/select"
 
 type CurrencyProps = {
-    currency?: "ALL" | "USD" | "Euro";
-    chooseCurrency?: () => void;
+    onCurrencyChange: (currency: string) => void;
 }
-const Currency = ({ chooseCurrency }: CurrencyProps) => {
+const Currency = ({ onCurrencyChange }: CurrencyProps) => {
     const [value, setValue] = useState('');
 
-    const currency = [{ALL: "ALL"},
-                      {USD: "USD"},
-                      {Euro: "Euro"}]
-        
-
-    
+    const handleCurrencyChange = (currency: string) => {
+        setValue(currency);
+        onCurrencyChange(currency);
+    }
     return (
         <div className="fixed top-12 left-14 sm:left-32 md:left-24 lg:left-32 xl:left-60 z-50 bg-none border-2 border-black text-xl text-black py-1 px-10 rounded-xl m-5 hover:bg-gradient-to-r hover:from-[#000080] hover:to-[#0000CD] hover:border-none hover:text-white">
             <Dialog>
@@ -43,16 +40,16 @@ const Currency = ({ chooseCurrency }: CurrencyProps) => {
                         </DialogDescription>
                     </DialogHeader>
                     <div>
-                        <Select value={value} onValueChange={setValue}>
+                        <Select value={value} onValueChange={handleCurrencyChange}>
                             <SelectTrigger className="w-[180px]">
-                                <SelectValue>
-                                    {currency[value]}
+                                <SelectValue placeholder="Currency">
+                                    {value}
                                 </SelectValue>
                             </SelectTrigger>
                             <SelectContent>
-                                <SelectItem value="ALL">ALL</SelectItem>
-                                <SelectItem value="USD">USD</SelectItem>
-                                <SelectItem value="Euro">Euro</SelectItem>
+                                <SelectItem value="Lek">ALL</SelectItem>
+                                <SelectItem value="$">USD</SelectItem>
+                                <SelectItem value="€">Euro</SelectItem>
                             </SelectContent>
                         </Select>
                     </div>
