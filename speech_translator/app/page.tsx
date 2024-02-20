197054dc0c4d3417d1af1836a690ac41f758ebc7
IjonK007 <kushta.joni@gmail.com>
2024-02-20 18:59:31 +0100

    Project Started

---
+++ speech_translator/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    
+    <main>
+      <h1 className='text-6xl font-bold '>
+        <span className='text-5xl font-bold bg-gradient-to-r from-[#FF002B] to-[#0086D1] bg-clip-text text-transparent'>Speech</span> <span className='text-5xl font-bold bg-gradient-to-r from-[#0086D1] to-[#FF002B] bg-clip-text text-transparent'>Translator</span>
+      </h1>
+    </main>
+  )
+}
+
+export default Home
---
+++ speech_translator/components/LanguagePicker.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  
+
+const LanguagePicker = () => {
+  return (
+    <div>
+        <Select>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+        </Select>
+        
+        <Select>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+        </Select>
+  </div>
+  )
+}
+
+export default LanguagePicker
