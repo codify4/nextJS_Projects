c9c5a72a89c7a9fd42d5cab3bf5aef785670be86
IjonK007 <kushta.joni@gmail.com>
2024-02-25 10:17:44 +0100

    Update Translate.tsx

--- speech_translator/components/Translate.tsx
+++ speech_translator/components/Translate.tsx
@@ -4,17 +4,42 @@ import { useState } from "react";
 import LanguagePicker from "./LanguagePicker";
 import translate from "google-translate-api";
 
-// make the design of the thing { text boxes }
 // lang picker 2 lang states props??
 // translate library use the states in the translate()
 
 
 const Translate = () => {
+
+    const [fromLang, setFromLang] = useState('');
+    const [toLang, setToLang] = useState('');
+
+    const handleLangSelect = (lang: string, name: string) => {
+        if(name === "From") {
+            setFromLang(lang);
+          }
+          else if(name === "To") {
+            setToLang(lang);
+          }
+    }
+
+    
     return (
-        <div>
+         <div className="flex flex-col items-center">
+
             <div className='inline-flex items-center justify-center mt-5 w-[500px] bg-neutral-800 rounded-xl'>
-                <LanguagePicker name='From' />
-                <LanguagePicker name='To' />
+                <LanguagePicker name='From' onLangSelect={handleLangSelect} />
+                <LanguagePicker name='To' onLangSelect={handleLangSelect} />
+            </div>
+
+            <div className="flex items-center justify-centerw-[800px] h-[300px] m-6 px-3 py-2 bg-neutral-800 rounded-[20px]">
+                <div className="inline-flex gap-6">
+                    <div className="flex w-[360px] h-[280px] p-4 rounded-[15px] bg-neutral-700 text-white">
+                        how you doing
+                    </div>
+                    <div className="flex w-[360px] h-[280px] p-4 rounded-[15px] bg-neutral-700 text-white">
+                        shka bane e la
+                    </div>
+                </div>
             </div>
         </div>
     );
