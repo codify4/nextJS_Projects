4ec95b0c9343ce1061e3b5f1da007f8accd48a4e
IjonK007 <kushta.joni@gmail.com>
2024-03-15 09:10:10 +0100

    Navigation added

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <p>Dashboard Page</p>
+  )
+}
+
+export default page
--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -28,7 +28,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
@@ -48,3 +47,5 @@ export default function Page() {
     </main>
   );
 }
+
+
